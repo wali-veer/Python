c51890730a3a1bedafac6873ed39c3562ed2b77d
Veeresh Wali <wali.veer@gmail.com>
2021-07-20 23:56:27 +0530

    Numerical ops

---
+++ 2.0/003.Bool_Logicl.py
@@ -0,0 +1,14 @@
+#Booleans - logical operations
+
+print((1 == 1) and (2 == 2)) #true
+print((1 == 1) or (2 == 2)) #true
+print (not(1 == 1)) #False
+print(not(1 == 2)) #true
+print(bool(None)) #False
+print(bool(0)) #False
+print(bool(2))#true
+print(bool("router"))#true
+print("\n\n")
+
+
+#None, 0, 0.0, 0L, 0j, empty string, empty list, empty tuple, empty dictionary #these values always evaluate to False
