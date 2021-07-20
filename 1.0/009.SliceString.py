4ac94dfd9e26b4213f5df23dbae1b950a3ef2622
Veeresh Wali <wali.veer@gmail.com>
2021-07-20 22:54:00 +0530

    strings

---
+++ 1.0/009.SliceString.py
@@ -0,0 +1,18 @@
+#StrVar[start : stop : step]
+
+
+str = "0123456789"
+print(str[1:5]) # 1234
+print(str[5:]) #56789
+print(str[:7]) #0123456
+print(str[-4:]) #6789
+print(str[:-6]) #0123
+print(str[-6:-1]) #45678
+print("output "+ str[-1:-6]+" done") #output  done
+print(str[::2]) #02468
+print(str[::-1]) #9876543210
+print(str[::2]) #02468
+print(str[1::2]) #13579
+print(str[1:7:2]) #135
+
+print("\n\n")
